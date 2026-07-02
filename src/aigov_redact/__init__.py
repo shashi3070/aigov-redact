47ee1e7b0cbd08a1175c260d0115ac73841af4f4
shashi kundan <rk683420@gmail.com>
2026-07-02 06:49:47 +0530

    v0.1.6: author metadata, expanded keywords, license-files, classifiers, documentation URLs

--- src/aigov_redact/__init__.py
+++ src/aigov_redact/__init__.py
@@ -1,3 +1,7 @@
+__version__ = "0.1.6"
+__author__ = "Shashi Kundan"
+__email__ = "shashikundan0001@gmail.com"
+
 from aigov_redact.models import AuditEntry, DetectionResult, PIIEntity, RedactResult
 from aigov_redact.redactor import detect, mask, redact
 
